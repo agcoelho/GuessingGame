a04134a038592091073e05b7d81293666d92ab18
agcoelho <afonsogcoelho@gmail.com>
2024-07-16 16:11:25 -0400

    ajustando os prints e loopando

--- guess.py
+++ guess.py
@@ -1,19 +1,19 @@
 print('###############################')
 print('Wellcome to the guessing game!')
 print('###############################')
-
+guess = 0
 secret_number = 42
+while guess != secret_number:
+    guess = int(input('Type your guess:\n'))
 
-guess = int(input('type your guess:\n'))
+    print('\nYou guessed:', guess,'\n')
 
-print('\nyou guessed:', guess,'\n')
+    if secret_number == guess:
+        print('You won. Congratulations\n')
+    else:
+        if guess > secret_number:
+            print('Your guess is above the right number\n Pls try again.')
 
-if secret_number != guess:
-    print('\nyou loose.\ngood luck next time.\n')
-else:
-    if guess > secret_number:
-        print('your guess is above the right number')
+        if guess < secret_number:
+            print('Your guess is under the right number\nPls try again')
 
-    if guess < secret_number:
-        print('your guess is under the right number')
-        
