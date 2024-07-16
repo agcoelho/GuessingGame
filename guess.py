08733617c0a29875264a2cd233e06d5281abf9c4
agcoelho <afonsogcoelho@gmail.com>
2024-07-16 16:33:59 -0400

    organizando o while

--- guess.py
+++ guess.py
@@ -3,7 +3,11 @@ print('Wellcome to the guessing game!')
 print('###############################')
 guess = 0
 secret_number = 42
-while guess != secret_number:
+above = guess > secret_number
+under = guess < secret_number
+attempts = 5
+while attempts > 0 and guess != secret_number:
+    print('attempts left:', attempts)
     guess = int(input('Type your guess:\n'))
 
     print('\nYou guessed:', guess,'\n')
@@ -11,9 +15,18 @@ while guess != secret_number:
     if secret_number == guess:
         print('You won. Congratulations\n')
     else:
-        if guess > secret_number:
-            print('Your guess is above the right number\n Pls try again.')
-
-        if guess < secret_number:
-            print('Your guess is under the right number\nPls try again')
-
+        if above:
+            print('Your guess is above the right number.\n')
+            attempts = attempts - 1
+            if attempts > 0:
+                print('Pls try again')
+            else:
+                print('you loose')
+        elif under:
+            print('Your guess is under the right number.\n')
+            attempts = attempts - 1
+            if attempts > 0:
+                print('Pls try again')
+            else:
+                print('you loose')
+            
